33ce909452b9a23fd7156351f39b48230785c49e
David Gao <david.04.gao@gmail.com>
2024-09-11 16:52:06 -0400

    part 1 commit

---
+++ A2MoreStuff/Part1/Part1/Part1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+void functionOneFirstHalf() {
+    int a, b, c, d;
+    a = 3;
+    b = a++ + ++a;
+    c = a++ + ++b;
+    cout << a << b << c << endl; 
+}
+
+void functionOneSecondHalf() {
+    int a, b, c;
+    a = 5;
+    b = 7;
+    c = a + 4 < b;
+    cout << a << b << c << endl;
+}
+
+void functionTwo() {
+    int a, b, c, d;
+    a = 5;
+    b = 2;
+    c = a < b;
+    cout << c << endl;
+    c = a < b + 4;
+    cout << c << endl;
+    d = a < b++;
+    cout << a << b << c << d << endl;
+    c = a + b++ + c++;
+    d = ++a + b++ + ++c;
+    cout << a << b << c << d << endl;
+}
+
+void functionThree() {
+    int a, b, * p, * q, * r;
+    a = 3;
+    b = 6;
+    p = &b;
+    q = &a;
+    *p = *q + 3;
+    r = p;
+    p = q;
+    q = r;
+    cout << a << b << *p << *q << *r << endl;
+}
+
+void functionFour() {
+    int a, b = 7;
+    int* p, * q, ** r, *** s;
+    s = &r;
+    r = &q;
+    p = &a;
+    r = &p;
+    a = 5;
+    q = &b;
+    //***r = *p - 6; I GET AN ERROR WHEN I TRY TO RUN THIS
+    cout << a << b << *p << *q << **r << ***s << endl;
+}
+
+
+int main()
+{
+    functionOneFirstHalf();
+    cout << endl;
+    functionOneSecondHalf();
+    cout << endl;
+    functionTwo();
+    cout << endl;
+    functionThree();
+    cout << endl;
+    functionFour();
+}
