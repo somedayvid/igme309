bc92066c8b268caf9cdfcf13e3b817393024efdc
David Gao <david.04.gao@gmail.com>
2024-09-27 00:18:24 -0400

    part 6 is done commented and displayed in main

--- A2MoreStuff/Part6/Matrix.h
+++ A2MoreStuff/Part6/Matrix.h
@@ -40,9 +40,7 @@ inline Matrix<T>::Matrix(T myElements[], int numRows, int numElements)
 {
 	heapArray = myElements;
 	rows = numRows;
-	
 	columns = numElements/ rows;	
-
 	size = numElements;
 }
 
@@ -64,12 +62,15 @@ inline Matrix<T>::Matrix(const Matrix& other)
 	rows = other.rows;
 	columns = other.columns;
 	size = other.size;
-	heapArray = new T[size];
+	T* newHeapArray = new T[size];
 
 	//adding all the elements in the original array to the new one
 	for (unsigned short i = 0; i < size; ++i) {
-		heapArray[i] = other.heapArray[i];
+		newHeapArray[i] = other.heapArray[i];
 	}
+
+	delete[] heapArray;
+	heapArray = newHeapArray;
 }
 
 /// <summary>
@@ -91,16 +92,16 @@ inline Matrix<T>& Matrix<T>::operator+(const Matrix& other)
 		return *newMatrix;
 	}
 	else {
-		std::cout << "Row and column sizes need to match!" << std::endl;
+		throw std::invalid_argument("Row and column size needs to match!");
 	}
 }
 
 /// <summary>
-/// 
+/// Subtraction operator which just subtracts each corresponding element in the matrix with the one in the other matrix
 /// </summary>
-/// <typeparam name="T"></typeparam>
-/// <param name="other"></param>
-/// <returns></returns>
+/// <typeparam name="T">Template Generic Class</typeparam>
+/// <param name="other">The other matrix to subtract from this one</param>
+/// <returns>A new matrix with subtracted values</returns>
 template<class T>
 inline Matrix<T>& Matrix<T>::operator-(const Matrix& other)
 {
@@ -114,40 +115,35 @@ inline Matrix<T>& Matrix<T>::operator-(const Matrix& other)
 		return *newMatrix;
 	}
 	else {
-		std::cout << "Row and column sizes need to match!" << std::endl;
+		throw std::invalid_argument("Row and column size needs to match!");
 	}
 }
 
 /// <summary>
-/// 
+/// Multiplication operator which performs matrix multiplication
 /// </summary>
-/// <typeparam name="T"></typeparam>
-/// <param name="other"></param>
-/// <returns></returns>
+/// <typeparam name="T">Template Generic Type</typeparam>
+/// <param name="other">The other matrix to be multiplied with</param>
+/// <returns>A new matrix with the new size and dimensions and dot product values</returns>
 template<class T>
 inline Matrix<T>& Matrix<T>::operator*(const Matrix& other)
 {
 	if (columns == other.rows) {
 		T* newArray = new T[rows * other.columns]{ NULL };
 		for (int i = 0; i < rows * other.columns; ++i) {
-			std::cout << "i count: " << i << std::endl;
 			T tempDotProduct = 0;
 			for (int j = 0; j < columns; ++j) {
-				int x = 0; // j = 0
+				int x = 0; 
 				int y = 0;
 
-				x = j; //0
-				if (i > other.columns - 1) { //4 > 2 yes
-					x += i/other.columns * columns; // 0 += 3 * 4/3 ---> 3
+				x = j;
+				if (i > other.columns - 1) { 
+					x += i / other.columns * columns;
 				}
 
-
-				y = j * other.columns + i % other.columns; // y = 6 + 1 ----> 7
+				y = j * other.columns + i % other.columns;
 								
-
-
 				tempDotProduct += heapArray[x] * other.heapArray[y];
-				std::cout << "       j count: " << j << " is: " << heapArray[x] << " " << other.heapArray[y] << std::endl;
 			}
 			for (int k = 0; k < rows * other.columns; ++k) {
 				if (newArray[k] == NULL) {
@@ -158,6 +154,9 @@ inline Matrix<T>& Matrix<T>::operator*(const Matrix& other)
 		}
 		Matrix<T>* newMatrix = new Matrix<T>(newArray, rows, rows * other.columns);
 		return *newMatrix;
+	}
+	else {
+		throw std::invalid_argument("This matrix's columns need to match the other's rows!");
 	}
 }
 
@@ -193,10 +192,10 @@ inline Matrix<T>& Matrix<T>::operator=(const Matrix& other)
 }
 
 /// <summary>
-/// 
+/// Scales each value in the matrix by the scalar
 /// </summary>
-/// <typeparam name="T"></typeparam>
-/// <param name="scalar"></param>
+/// <typeparam name="T">Template Generic Type</typeparam>
+/// <param name="scalar">The number amount to scale by</param>
 template<class T>
 inline void Matrix<T>::scale(T scalar)
 {
@@ -206,9 +205,9 @@ inline void Matrix<T>::scale(T scalar)
 }
 
 /// <summary>
-/// 
+/// Transposes a matrix, swapping the rows and columns
 /// </summary>
-/// <typeparam name="T"></typeparam>
+/// <typeparam name="T">Generic Template Type</typeparam>
 template<class T>
 inline void Matrix<T>::transpose()
 {
@@ -235,6 +234,11 @@ inline void Matrix<T>::transpose()
 	newArray = nullptr;
 }
 
+/// <summary>
+/// Operator linking thing so the determinant function can be called with default parameters from the matrix it is being called from
+/// </summary>
+/// <typeparam name="T">Generic Template Type</typeparam>
+/// <returns>The determinant once the other function runs through</returns>
 template<class T>
 inline T Matrix<T>::determinant()
 {
@@ -242,7 +246,7 @@ inline T Matrix<T>::determinant()
 }
 
 /// <summary>
-/// Calculates and returns the determinant of this matrix
+/// Recursive function to calculate the determinant of a matrix
 /// </summary>
 /// <typeparam name="T">Generic Template Type</typeparam>
 /// <param name="incomingArray">The array of matrix numbers</param>
@@ -296,18 +300,81 @@ inline T Matrix<T>::determinant(T incomingArray[], const int incomingRows, const
 	}
 }
 
+/// <summary>
+/// Inverses this matrix
+/// </summary>
+/// <typeparam name="T">Template Generic Type</typeparam>
 template<class T>
 inline void Matrix<T>::inverse()
 {
-	T* tempArray = new T[size]{ heapArray[3], -heapArray[1], -heapArray[2], heapArray[0] };
-	for (int i = 0; i < size; ++i) {
-		tempArray[i] /= determinant(heapArray, 2, 2);
+	if (determinant() != 0) {
+		transpose();	
+		T* newArray;
+
+		if (rows == 2 && columns == rows) {
+			newArray = new T[size]{ heapArray[3], -heapArray[1], -heapArray[2], heapArray[0] };
+			for (int i = 0; i < size; ++i) {
+				newArray[i] /= determinant(heapArray, 2, 2);
+			}
+		}
+		else {
+			newArray = new T[rows * columns];
+			for (int m = 0; m < rows * columns; ++m) {
+				newArray[m] = std::numeric_limits<T>::min();
+			}
+
+			int tempArraySize = (columns - 1) * (rows - 1);
+
+			for (int i = 0; i < rows * columns; ++i) {
+
+				T* tempArray = new T[tempArraySize];
+				for (int m = 0; m < tempArraySize; ++m) {
+					tempArray[m] = std::numeric_limits<T>::min();
+				}
+
+				for (int j = 0; j < rows * columns; ++j) {
+					if ((j / columns) != (i / columns) && (j % columns != i % columns)) {
+						for (int l = 0; l < tempArraySize; ++l) {
+							if (tempArray[l] == std::numeric_limits<T>::min()) {
+								tempArray[l] = heapArray[j];
+								break;
+							}
+						}
+					}
+				}
+
+				for (int l = 0; l < rows * columns; ++l) {
+					if (newArray[l] == std::numeric_limits<T>::min()) {
+						newArray[l] = determinant(tempArray, rows - 1, columns - 1);
+						break;
+					}
+				}
+
+				delete[] tempArray;
+				tempArray = nullptr;
+			}
+
+			double mynewDeterminant = determinant();
+			for (int i = 0; i < rows * columns; ++i) {
+				if (i % 2 != 0) {
+					newArray[i] *= -1;
+				}
+				newArray[i] /= mynewDeterminant;
+			}
+		}
+
+		delete[] heapArray;
+		heapArray = newArray;
+		newArray = nullptr;
 	}
-	delete[] heapArray;
-	heapArray = tempArray;
-	tempArray = nullptr;
 }
 
+/// <summary>
+/// Multiplies a matrix by a 2 or 3d vector
+/// </summary>
+/// <typeparam name="T">Generic Template Type</typeparam>
+/// <param name="other">The vector to multiply the matrix with</param>
+/// <returns>A matrix with one column size and rows equal to this</returns>
 template<class T>
 inline Matrix<T>& Matrix<T>::multiplyByVector(Vector<T>& other)
 {
@@ -344,6 +411,9 @@ inline Matrix<T>& Matrix<T>::multiplyByVector(Vector<T>& other)
 		}
 		Matrix<T>* newMatrix = new Matrix<T>(newArray, rows, other.getSize());
 		return *newMatrix;
+	}
+	else {
+		throw std::invalid_argument("Matrix's column size needs to match vector's number of elements!");
 	}
 }
 
--- A2MoreStuff/Part6/Part6.cpp
+++ A2MoreStuff/Part6/Part6.cpp
@@ -10,45 +10,96 @@ using namespace std;
 
 float main()
 {
-	//Vector<float>* myVect = new Vector<float>(4,3,1);
-	//Vector<float>* myVect2 = new Vector<float>(5, 10);
+	cout << "Int Vector testing" << endl;
+	Vector<float>* floatVector = new Vector<float>(1, 2, 3);
+	Vector<float>* otherFloatVector = new Vector<float>(3, 2, 1);
 
-	//myVect->print();
-	//myVect2->print();
-	//Vector<float>* whatVector = new Vector<float>(myVect->operator+(*myVect2));
-	//whatVector->print();
+	cout << "Vector Testing: " << endl;
+	cout << "floatVector: " << endl;
+	floatVector->print();
 
-	////Vector<float>* myVect3 = new Vector<float>(20, 50);
+	cout << endl << "otherFloatVector: " << endl;
+	otherFloatVector->print();
 
-	//whatVector->operator=(myVect->operator+=(*myVect2));
-	//whatVector->print();
-	//myVect->operator+=(*whatVector);
-	//myVect->print();
+	cout << endl << "Adding otherFloatVector to floatVector: " << endl;
+	floatVector->operator+=(*otherFloatVector);
 
-	////whatVector->normalize();
-	////whatVector->print();
-	////whatVector->addDimension(20);
-	////whatVector->print();
+	floatVector->print();
 
-	//std::cout << sizeof(myaayy);
+	cout << endl << "Subtracting otherFloatVector from floatVector: " << endl;
+	floatVector->operator-=(*otherFloatVector); 
 
-	Matrix<float>* myMatrix = new Matrix<float>(new float[9]{1,2,3,4,5,6,7,8,9}, 3, 9);
-	myMatrix->print();
-	Matrix<float>* matrix2 = new Matrix<float>(new float[9]{9,8,7,6,5,4,3,2,1}, 3, 9);
+	floatVector->print();
 
-	//Matrix<float>* newerMatrix = new Matrix<float>(myMatrix->multiplyByVector(*myVect));
-	myMatrix->operator=(myMatrix->operator*(*matrix2));
-	//newerMatrix->print();
-	myMatrix->print();
-	//myMatrix->inverse();
-	////myMatrix->transpose();
-	//myMatrix->print();
-	//myMatrix->print();
-	//myMatrix->transpose();
-	//myMatrix->print();
+	cout << endl << "Scaling floatVector by 3" << endl;
+	floatVector->scale(3);
 
-	//Matrix<float>* newMatrix = new Matrix<float>(new float[4] {1,2,3,4}, 2, 4);
-	//cout << newMatrix->determinant();
+	floatVector->print();
 
+	cout << endl << "Subtracting otherFloatVector from floatVector" << endl;
+	floatVector->operator-=(*otherFloatVector);
 
+	floatVector->print();
+
+	cout << endl << "Getting the dot product between floatVector and otherFloatVector: " << endl;
+	cout << floatVector->dot(*otherFloatVector);
+
+	cout << endl<<endl << "Setting floatVector equal to the cross product between floatVector and otherFloatVector: " << endl;
+	floatVector->operator=(floatVector->cross(*otherFloatVector));
+
+	floatVector->print();
+
+	cout << endl << "Normalizing floatVector: " << endl;
+	floatVector->normalize();
+
+	floatVector->print();
+
+	cout <<endl<< "Matrix testing: " << endl;
+	Matrix<float>* floatMatrix = new Matrix<float>(new float[9] {1,2,3,4,5,6,7,8,9}, 3, 9);
+	Matrix<float>* otherFloatMatrix = new Matrix<float>(new float[9] {7,2,3,9,8,10,14, 13, 4}, 3, 9);
+	cout <<endl<< "floatMatrix: " << endl;
+	floatMatrix->print();
+	cout <<endl << endl<< "otherFloatMatrix: " << endl;
+	otherFloatMatrix->print();
+
+	cout <<endl<<endl<< "Adding floatMatrix and otherFloatMatrix together: " << endl;
+	floatMatrix->operator=(floatMatrix->operator+(*otherFloatMatrix));
+
+	floatMatrix->print();
+
+	cout <<endl<<endl<< "Subtracting otherFloatMatrix from floatMatrix: " << endl;
+	floatMatrix->operator=(floatMatrix->operator-(*otherFloatMatrix));
+
+	floatMatrix->print();
+	otherFloatMatrix->print();
+
+	cout <<endl<<endl<< "Multiplying floatMatrix and otherFloatMatrix: " << endl;
+	floatMatrix->operator=(floatMatrix->operator*(*otherFloatMatrix));
+
+	floatMatrix->print();
+
+	cout <<endl<<endl<< "Multiplying floatMatrix by otherFloatVector: " << endl;
+	Matrix<float>* matrixTimesVector = new Matrix<float>(floatMatrix->multiplyByVector(*otherFloatVector));
+
+	matrixTimesVector->print();
+
+	cout << endl << endl << "Scaling floatMatrix by 3: " << endl;
+	floatMatrix->scale(3);
+
+	floatMatrix->print();
+
+	cout <<endl<<endl<< "Determinant of otherFloatMatrix:  " << endl;
+	cout << otherFloatMatrix->determinant() << endl;
+
+	otherFloatMatrix->print();
+
+	cout <<endl<<endl<< "Transpose otherFloatMatrix: " << endl;
+	otherFloatMatrix->transpose();
+
+	otherFloatMatrix->print();
+
+	cout <<endl<<endl<< "Inverse otherFloatMatrix: " << endl;
+	otherFloatMatrix->inverse();
+
+	otherFloatMatrix->print();
 }
